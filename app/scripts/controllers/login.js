3d0fb0aef90fe30b323d8d236a7b0460a46c7843
Vaibhav kohli <vaibhav.khl@gmail.com>
2014-03-31 23:03:57 +0530

    login ctrl and local users json file

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('angularDemoApp')
+  .controller('LoginCtrl', function ($scope,Data) {
+    Data.get_local('scripts/lib/users.json').success(function(users_data){
+    $scope.users_data = users_data[users][0]
+  });
